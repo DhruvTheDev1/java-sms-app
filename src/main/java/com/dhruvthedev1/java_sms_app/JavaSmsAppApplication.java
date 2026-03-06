678187da30a6a937ca889f61f03ac9e1e9c397e4
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2026-03-06 14:56:01 +0000

    Initial commit

---
+++ src/main/java/com/dhruvthedev1/java_sms_app/JavaSmsAppApplication.java
@@ -0,0 +1,13 @@
+package com.dhruvthedev1.java_sms_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSmsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhruvthedev1/java_sms_app/JavaSmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruvthedev1.java_sms_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSmsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

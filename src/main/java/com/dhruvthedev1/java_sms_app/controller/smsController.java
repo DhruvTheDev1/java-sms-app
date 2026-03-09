43d4f7197880a301ecd59e11b1a5bc178c00cc9d
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2026-03-09 14:59:59 +0000

    Add: smsService Class - Uses toNumber (recipient's number) and fromNumber to send a sms. - Using Twilio

---
+++ src/main/java/com/dhruvthedev1/java_sms_app/controller/smsController.java
@@ -0,0 +1,10 @@
+package com.dhruvthedev1.java_sms_app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class smsController {
+  
+  
+
+}
---
+++ src/main/java/com/dhruvthedev1/java_sms_app/service/smsService.java
@@ -0,0 +1,32 @@
+package com.dhruvthedev1.java_sms_app.service;
+
+import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class smsService {
+  private String accountSID;
+  private String authToken;
+  private String fromNumber;
+  private String toNumber;
+
+  @PostConstruct
+  public void initialise() {
+    Twilio.init(accountSID, authToken);
+  }
+
+  public String sendMessage(String toNumber, String messageBody) {
+    Message message = Message.creator(
+      new PhoneNumber(toNumber),
+       new PhoneNumber(fromNumber),
+        messageBody
+        ).create();
+        
+        return message.getStatus().toString();
+  }
+}
